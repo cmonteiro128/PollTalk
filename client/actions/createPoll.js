ce950a00d04ee78fd0512d285f0d105b3d4d09d6
tenhagena <tenhagena@wit.edu>
2018-03-28 16:20:42 -0400

    Legendary webdev fixed the erros

--- client/actions/createPoll.js
+++ client/actions/createPoll.js
@@ -20,9 +20,14 @@ export function addPollName(option) {
 
 export async function createPoll(pollOptions, pollName) {
   const dataToPass = { pollOptions, pollName };
+  console.log(JSON.stringify(dataToPass));
   const pollID = await fetch('http://localhost:5000/createPoll', {
     method: 'POST',
     body: JSON.stringify(dataToPass),
+    mode: 'cors',
+    headers: {
+      'content-type': 'application/json',
+    },
   });
   return {
     type: CREATE_POLL,
